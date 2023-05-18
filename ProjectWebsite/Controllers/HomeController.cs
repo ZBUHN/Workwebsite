606fc85074a7800d7070848cf5b3ac71fa8e888f
Zabnullah <z7777075995@gmail.com>
2023-05-18 14:52:05 +0300

    Final Commit

---
+++ ProjectWebsite/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectWebsite.Models;
+using System.Diagnostics;
+
+namespace ProjectWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Blog()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Services()
+        {
+            return View();
+        }
+        public IActionResult Single()
+        {
+            return View();
+        }
+
+
+
+    }
+}
